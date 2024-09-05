003b3f5b96bbfeafedfb4026c1df3b73378bec14
Vladislav Cernega <vladislav@walkcat.net>
2024-09-05 21:22:34 +0100

    add debug

--- popup.js
+++ popup.js
@@ -4,12 +4,27 @@ document.getElementById('fillFormBtn').addEventListener('click', () => {
     const endMonth = document.getElementById('endMonth').value;
     const amount = document.getElementById('amount').value;
 
+    console.log('Button pressed. Sending the following data to content script:');
+    console.log({ option, startMonth, endMonth, amount });
+
+    // Make sure the data is valid before sending the message
+    if (!option || !startMonth || !endMonth || !amount) {
+        console.error('Some fields are missing!');
+        alert('Please fill in all the fields.');
+        return;
+    }
+
+    // Send the message to the content script
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.tabs.sendMessage(tabs[0].id, {
             action: 'fillForm',
             data: { option, startMonth, endMonth, amount }
         }, (response) => {
-            console.log(response.status);
+            if (response) {
+                console.log(response.status);
+            } else {
+                console.error('No response from content script.');
+            }
         });
     });
 });
